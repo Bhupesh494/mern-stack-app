be3175ca7c14f70b3f5dbc6deb282bae99f367c6
BhupeshRajpurohit <bhupesh.rajpurohit@crossasyst.com>
2022-06-18 21:52:19 +0530

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 
 const express = require('express');
 
+const workoutRoutes = require('./routes/workouts');
+
+// express app
 const app = express();
+
+// middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.json({ message: 'jai maa serra vaali' });
-});
+// routes
+app.use('/api/workouts', workoutRoutes);
 
+// listen for requests
 app.listen(process.env.PORT, () => {
-  return console.log('Listening on port ', process.env.PORT);
+  console.log('listening on port', process.env.PORT);
 });
